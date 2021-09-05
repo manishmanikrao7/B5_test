3b6246ca8ac5275f8ddd240b23288b874604baa6
manishmanikrao7 <manishpatil71192@gmail.com>
2021-09-05 19:15:38 +0530

    added a comment

--- boards/apps.py
+++ boards/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class BoardsConfig(AppConfig):
+    """this is a boards config"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'boards'
